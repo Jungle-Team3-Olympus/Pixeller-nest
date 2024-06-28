e392f06f3cc2ae833494a6b79f512d58ed5f3756
Digi24-dev <digi7442@gmail.com>
2024-06-28 23:02:40 +0900

    <UPDATE> -  서버 clientId 기준 업데이트로 변경

--- src/chat/chat/chat.gateway.ts
+++ src/chat/chat/chat.gateway.ts
@@ -12,47 +12,69 @@ export class ChatGateway {
   @WebSocketServer()
   server: Server;
 
+  /**
+   * 사용자 정보를 저장할 배열
+   * @type {JSON[]}
+   * @memberof ChatGateway
+   * @example { username: string, x: number, y: number, client: string, direction: string }
+   * @description 사용자 정보를 저장할 배열
+   */
   users: {}[] = [];
 
   constructor() {
-    setInterval(() => {
-      this.server.emit('message', { type: 'move', users: this.users });
-    }, 10); // 5초마다 메시지를 보냅니다.
+    // setInterval(() => {
+    //   this.server.emit('message', { type: 'move', users: this.users });
+    // }, 10); // 5초마다 메시지를 보냅니다.
   }
 
+  /**
+   * @description 클라이언트가 연결되었을 때 실행되는 메서드 + 클라이언트에게 uid 전송, type: connect
+   * @param client client socket
+   */
   @SubscribeMessage('connect')
   handleConnection(client: Socket): void {
     console.log(`client connected ${client.id}`);
-    // send msg except just connected client
-    client.broadcast.emit('message', { uid: `${client.id}`, type: 'newplayer' });
+    // send msg to client
+    client.emit('message', { uid: `${client.id}`, type: 'connect' });
   }
 
   @SubscribeMessage('message')
-  handleMessage(@MessageBody() message: string, client: Socket): void {
+  handleMessage(@MessageBody() message: string): void {
     this.server.emit('message', message);
   }
 
   @SubscribeMessage('join')
-  handleJoin(@MessageBody() data: { username: string }, client: Socket): void {
-    console.log(this.users);
+  handleJoin(@MessageBody() data: { username: string }): void {
+    // console.log(this.users);
     const welcomeMessage = `${data.username} has joined the chat`;
+
     // 사용자 정보를 users 배열에 추가
     this.users.push({ username: data.username, x: 1, y: 1 });
     this.server.emit('message', { type: 'newplayer', text: welcomeMessage });
   }
 
   @SubscribeMessage('leave')
-  handleLeave(@MessageBody() data: { username: string }, client: Socket): void {
+  handleLeave(@MessageBody() data: { username: string; clientId: string }): void {
     const farewellMessage = `${data.username} has left the chat`;
+
+    // 사용자 정보를 users 배열에서 삭제, username으로 삭제
+    // this.users = this.users.filter((user: { username: string }) => user.username !== data.username);
+    // 사용자 정보를 users 배열에서 삭제, clientId로 삭제
+    this.users = this.users.filter((user: { clientId: string }) => user.clientId !== data.clientId);
+
     this.server.emit('message', farewellMessage);
   }
+
   @SubscribeMessage('move')
-  handleMove(@MessageBody() data: {}, client: Socket): void {
-    this.users.forEach((user: { username: string; x: number; y: number }) => {
-      if (user.username === data['username']) {
+  handleMove(@MessageBody() data: {}): void {
+    this.users.forEach((user: { username: string; x: number; y: number; clientId: string; direction: string }) => {
+      if (user.clientId === data['clientId']) {
         user.x = data['x'];
         user.y = data['y'];
+        user.direction = data['direction'];
+        user.username = data['username'];
       }
     });
+    this.server.emit('message', { type: 'move', users: this.users });
   }
 }

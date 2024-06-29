47e6133f5958dacc36064a43ad63519e11dd7c93
wooseong-choi <ghkdgo8686@gmail.com>
2024-06-29 16:57:40 +0900

    sss

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
     transform:true,
   }));
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://192.168.0.109:3000'],  // 원하는 도메인
+    origin: ['http://localhost:3000', 'http://192.168.0.96:3000', 'http://172.23.176.1:3000'],  // 원하는 도메인
     methods: ['GET', 'POST', 'PUT', 'DELETE'],  // 허용할 HTTP 메서드
     credentials: true,  // 클라이언트에서 인증정보(Cookie 등)를 전송할 수 있도록 설정
   });

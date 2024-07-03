2523ab58bbfb97eb2e22c1b47d636066a73bdf4e
wooseong-choi <ghkdgo8686@gmail.com>
2024-07-03 23:55:16 +0900

    설정파일 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,9 @@ import { UserService } from './user/user.service';
 import { UserController } from './user/user.controller';
 import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Module({
   imports: [
@@ -15,12 +18,12 @@ import { ConfigModule } from '@nestjs/config';
       isGlobal: true,
     }),
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'database-1.crqmagci2bfr.ap-northeast-2.rds.amazonaws.com',
-      port: 3306,
-      username: 'guardian',
-      password: 'dnflxlaWkd123',
-      database: 'olympus',
+      type: process.env.DB_TYPE as any,
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT, 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),

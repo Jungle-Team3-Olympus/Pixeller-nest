2ab20d60df808db27b31519897d7c4f6afbaa7cb
류강현 <rkdgus9394@gmail.com>
2024-07-23 22:34:58 +0900

    pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,18 @@
 module.exports = {
-    apps : [{
-      name: "nest-app",
-      script: "nest",
-      args: "start",
-      instances: "max",
-      exec_mode: "cluster",
-      watch: false,
-      max_memory_restart: "24G", // 인스턴스당 최대 메모리 사용량을 24GB로 설정
-      env: {
-        NODE_ENV: "production",
-      },
-      node_args: "--max-old-space-size=24576" // 24GB로 Node.js 메모리 제한 설정 (24 * 1024 = 24576)
-    }]
-  }
+  apps : [{
+    name: "nest-app",
+    script: "nest",
+    args: "start",
+    instances: 1,
+    exec_mode: "fork",
+    watch: false,
+    max_memory_restart: "28G",
+    env: {
+      NODE_ENV: "development",
+    },
+    env_production: {
+      NODE_ENV: "production",
+    },
+    node_args: "--max-old-space-size=28672"
+  }]
+}

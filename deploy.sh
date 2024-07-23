35468e29583f00169a9d26a625ae4f11b643ec36
류강현 <rkdgus9394@gmail.com>
2024-07-23 20:40:54 +0900

    why

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,6 @@ sudo npm install --legacy-peer-deps # 의존성 파일 설치.
 sudo npm install -g pm2@latest
 sudo pm2 stop nest-app || true # 기존에 실행 중인 프로세스가 있다면 중지
 sudo pm2 delete nest-app || true # 기존에 실행 중인 프로세스가 있다면 삭제
-# sudo pm2 start nest --name "nest-app" -- start # 애플리케이션에 이름을 부여하여 시작
-sudo pm2 start ecosystem.config.js
+sudo pm2 start nest --name "nest-app" -- start # 애플리케이션에 이름을 부여하여 시작
+# sudo pm2 start ecosystem.config.js
 sudo pm2 save # 현재 실행 중인 프로세스 목록 저장
